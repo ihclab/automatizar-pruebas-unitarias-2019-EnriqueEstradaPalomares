2f55561d6b2695a99626e01576304a072d9f947b
Your Name <jestrada14@ucol.com>
2019-03-21 17:04:49 -0600

    Problema 2(Cambios)

---
+++ Media.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebasUnitarias
+{
+    class Media
+    {
+        public static double MediaAritmetica (int[] vals)
+        {
+            double total = Convert.ToDouble(vals.Length);
+            double suma = 0.0;
+            for(int i = 0; i < vals.Length; i++)
+            {
+                suma += Convert.ToDouble(vals[i]);
+            }
+            return suma/total;
+        }
+
+        public double raizEnesima(double x, int n)
+        {
+            return Math.Pow(x, (1 / Convert.ToDouble(n)));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebasUnitarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Coloque el archivo CasosPrueba en C:");
+            Console.ReadLine();
+            Console.Clear();
+            StreamReader reader = new StreamReader(@"C:\CasosPrueba.txt");
+            string sLine = "";
+            ArrayList arrText = new ArrayList();
+            while (sLine != null)
+            {
+                sLine = reader.ReadLine();
+                if (sLine != null)
+                    arrText.Add(sLine);
+            }
+            reader.Close();
+
+            foreach (string sOutput in arrText)
+            Console.WriteLine(sOutput);
+            Console.ReadLine();
+        }
+    }
+}
