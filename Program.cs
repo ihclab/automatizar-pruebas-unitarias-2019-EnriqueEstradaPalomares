e5c4545925f2e55f24b6cac4b06818b5c56c0e10
EnriqueEstradaPalomares <36722043+EnriqueEstradaPalomares@users.noreply.github.com>
2019-03-21 00:27:36 -0600

    Problema 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebasUnitarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Media med = new Media();
+            int[] vec = { 1, 2, 3, 4 };
+            Console.WriteLine(Media.MediaAritmetica(vec).ToString());
+            Console.WriteLine(med.raizEnesima(3, 4));
+            Console.ReadKey();
+        }
+    }
+}
